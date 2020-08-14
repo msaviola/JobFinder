6ca26dad1fba4332b7ef3cd0facad8783c0297ba
msaviola <67287151+msaviola@users.noreply.github.com>
2020-08-13 18:47:08 -0700

    java script with Elma

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 
-
-/**
- * pulls information from the form and build the query URL
- * @returns {string} URL for NYT API based on form inputs
- */
-
-var queryURL = "https://jobs.github.com/positions.json?";
-
-var queryParams= "";
+// /**
+//  * pulls information from the form and build the query URL
+//  * @returns {string} URL for NYT API based on form inputs
+//  */
 
 
-//language description
+// var queryUrl = "https://api.indeed.com/ads/apisearch?publisher=123412341234123&q=java+developer&l=austin%2C+tx&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2"
+
+// // var queryParams= "";
+
+// $.ajax({
+//   url: queryUrl,
+//   method: "GET"
+// })
+// .then(function (response){
+
+//   // console.log(url)
+//   console.log(response)
+// })
+
+
+$.ajax({
+  url: `https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=pyyn&location=new+yk`,
+  method: "GET",
+}).then(function (response) {
+  console.log(response);
+})
+
+
+/* //language description
 queryParams.q = $("#search")
 .val()
 .trim();
@@ -36,3 +54,4 @@ console.log(queryURL + $.param(queryParams));
 
 
 
+ */
